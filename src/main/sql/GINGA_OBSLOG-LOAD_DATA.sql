2b6304e2fea9ab08f37e8cdb3cd6a0ab65c28bcc
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2014-11-24 01:09:19 +0100

    Create OBSLOG table and load data (includes hh:mm:ss in STARTTIME and ENDTIME columns plus extra columns, e.g. NUM_PASS, FLAG, PASS_NAMES)

---
+++ src/main/sql/GINGA_OBSLOG-LOAD_DATA.sql
@@ -0,0 +1,7 @@
+LOAD DATA LOCAL INFILE 'OBSLOG'  
+INTO TABLE ginga.GINGA_OBSLOG
+FIELDS TERMINATED BY '|' 
+LINES TERMINATED BY '\n'  
+IGNORE 1 LINES 
+(SEQ_NO, NUM_PASSES, @target_name_variable, AVE_ALPHA, AVE_DELTA, MAX_ALPHA, MAX_DELTA, MIN_ALPHA, MIN_DELTA, @flag_variable, DISTANCE, @start_date_variable, @start_time_variable, @end_date_variable, @end_time_variable, POINTING, @pass_names_variable)   
+SET TARGET_NAME=TRIM(@target_name_variable), FLAG=TRIM(@flag_variable), STARTTIME=STR_TO_DATE(CONCAT_WS(' ', @start_date_variable, @start_time_variable),'%y%m%d %h:%i:%s'), ENDTIME=STR_TO_DATE(CONCAT_WS(' ', @end_date_variable, @end_time_variable),'%y%m%d %h:%i:%s'), PASS_NAMES=TRIM(@pass_names_variable);
