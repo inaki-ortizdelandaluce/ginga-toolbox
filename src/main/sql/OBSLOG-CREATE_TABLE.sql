0025f8a2e9805493e02170ed555c977a09e32c43
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2014-11-25 00:09:43 +0100

    Fix bug with Hour in 0-23 format

--- src/main/sql/OBSLOG-CREATE_TABLE.sql
+++ src/main/sql/OBSLOG-CREATE_TABLE.sql
@@ -1,21 +1,21 @@
-   drop table if exists ginga.OBSLOG;
+    drop table if exists ginga.OBSLOG;
 
     create table ginga.OBSLOG (
         ID bigint not null auto_increment,
-        AVE_ALPHA float not null,
-        AVE_DELTA float not null,
-        DISTANCE float not null,
-        ENDTIME datetime not null,
-        FLAG varchar(4) not null,
-        MAX_ALPHA float not null,
-        MAX_DELTA float not null,
-        MIN_ALPHA float not null,
-        MIN_DELTA float not null,
-        NUM_PASSES integer not null,
-        PASS_NAMES varchar(100),
+        AVE_ALPHA float,
+        AVE_DELTA float,
+        DISTANCE float,
+        ENDTIME datetime,
+        FLAG varchar(4),
+        MAX_ALPHA float,
+        MAX_DELTA float,
+        MIN_ALPHA float,
+        MIN_DELTA float,
+        NUM_PASSES integer,
+        PASS_NAMES longtext,
         POINTING float,
-        SEQ_NO integer not null,
-        STARTTIME datetime not null,
-        TARGET_NAME varchar(80) not null,
+        SEQ_NO integer,
+        STARTTIME datetime,
+        TARGET_NAME varchar(80),
         primary key (ID)
     ) ENGINE=InnoDB;
