c48b34068219acdf178198957ef7b9e561cd5bf4
Inaki <inaki.ortizdelandaluce@gmail.com>
2014-12-01 13:16:53 +0100

    Added LAC MODE Constants

--- src/main/java/org/ginga/tools/util/Constants.java
+++ src/main/java/org/ginga/tools/util/Constants.java
@@ -2,16 +2,30 @@ package org.ginga.tools.util;
 
 public class Constants {
 
-	public enum BitRate { ANY, HI,  MID, LOW }
-	public enum LacMode { MPC1, MPC2, MPC3, PC, INIT }
-	public enum BgSubtractionMethod { SIMPLE, SUD_SORT, HAYASHIDA }
-	public enum LacCounterMode { MIDDLE, TOP, BOTH }
-	public enum TimeResolution { ONE, ONE_OVER_TWO, ONE_OVER_FOR }
-	
-	public static final double DEFAULT_MIN_ELEVATION = 5.0;
-	public static final double DEFAULT_MAX_ELEVATION = 180.0;
-	public static final double DEFAULT_MIN_RIGIDITY = 10.0;
-	public static final double DEFAULT_MAX_RIGIDITY = 20.0;
-	
+    public enum BitRate {
+        ANY, HI, MID, LOW
+    }
+
+    public enum LacMode {
+        MPC1, MPC2, MPC3, ACS, PCHK, PC, INIT
+    }
+
+    public enum BgSubtractionMethod {
+        SIMPLE, SUD_SORT, HAYASHIDA
+    }
+
+    public enum LacCounterMode {
+        MIDDLE, TOP, BOTH
+    }
+
+    public enum TimeResolution {
+        ONE, ONE_OVER_TWO, ONE_OVER_FOR
+    }
+
+    public static final double DEFAULT_MIN_ELEVATION = 5.0;
+    public static final double DEFAULT_MAX_ELEVATION = 180.0;
+    public static final double DEFAULT_MIN_RIGIDITY = 10.0;
+    public static final double DEFAULT_MAX_RIGIDITY = 20.0;
+    public static final BitRate DEFAULT_BIT_RATE = BitRate.HI;
 
 }
