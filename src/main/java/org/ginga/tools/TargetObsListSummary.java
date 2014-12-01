d164df0b5d16f612d4fda8ab33cec1e34f4db117
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2014-12-01 23:31:38 +0100

    Add observation summary methods for all/spectral/MPC2 modes

--- src/main/java/org/ginga/tools/TargetObsListSummary.java
+++ src/main/java/org/ginga/tools/TargetObsListSummary.java
@@ -7,10 +7,11 @@ import java.util.List;
 import org.ginga.tools.observation.ObservationEntity;
 import org.ginga.tools.observation.ObservationModeDetails;
 import org.ginga.tools.pipeline.TargetObservationListPipe;
+import org.ginga.tools.util.Constants.LacMode;
 
 public class TargetObsListSummary {
 
-    public void execute(String target) {
+    public void printAllModes(String target) {
         TargetObservationListPipe pipe = new TargetObservationListPipe();
         pipe.setStarts(Arrays.asList(target));
         List<ObservationEntity> obsList = pipe.next();
@@ -39,12 +40,77 @@ public class TargetObsListSummary {
         writer.close();
     }
 
+    public void printSpectralModes(String target) {
+        TargetObservationListPipe pipe = new TargetObservationListPipe();
+        pipe.setStarts(Arrays.asList(target));
+        List<ObservationEntity> obsList = pipe.next();
+
+        PrintWriter writer = new PrintWriter(System.out);
+        List<ObservationModeDetails> obsModeList = null;
+        writer.println("----------------------------------------------------------");
+        writer.println("----------------------------------------------------------");
+        for (ObservationEntity obsEntity : obsList) {
+            obsModeList = obsEntity.getAvailableModesDetails();
+            if (obsModeList != null) {
+                for (ObservationModeDetails obsMode : obsModeList) {
+                	LacMode mode = obsMode.getLacMode();
+                	if(mode.equals(LacMode.MPC1) || mode.equals(LacMode.MPC2)) {
+                        writer.println(" "
+                                + String.format(
+                                        "%18s",
+                                        obsEntity.getSequenceNumber() + " "
+                                                + String.format("%8s", obsMode.getMode()) + " "
+                                                + String.format("%20s", obsMode.getStartTime()) + " "
+                                                + String.format("%20s", obsMode.getEndTime())));
+                	}
+                }
+                writer.println("----------------------------------------------------------");
+            }
+        }
+        writer.println("----------------------------------------------------------");
+        writer.flush();
+        writer.close();
+    }
+
+    public void printMpc2Modes(String target) {
+        TargetObservationListPipe pipe = new TargetObservationListPipe();
+        pipe.setStarts(Arrays.asList(target));
+        List<ObservationEntity> obsList = pipe.next();
+
+        PrintWriter writer = new PrintWriter(System.out);
+        List<ObservationModeDetails> obsModeList = null;
+        writer.println("----------------------------------------------------------");
+        writer.println("----------------------------------------------------------");
+        for (ObservationEntity obsEntity : obsList) {
+            obsModeList = obsEntity.getAvailableModesDetails();
+            if (obsModeList != null) {
+                for (ObservationModeDetails obsMode : obsModeList) {
+                	LacMode mode = obsMode.getLacMode();
+                	if(mode.equals(LacMode.MPC2)) {
+                        writer.println(" "
+                                + String.format(
+                                        "%18s",
+                                        obsEntity.getSequenceNumber() + " "
+                                                + String.format("%8s", obsMode.getMode()) + " "
+                                                + String.format("%20s", obsMode.getStartTime()) + " "
+                                                + String.format("%20s", obsMode.getEndTime())));
+                	}
+                }
+                writer.println("----------------------------------------------------------");
+            }
+        }
+        writer.println("----------------------------------------------------------");
+        writer.flush();
+        writer.close();
+    }    
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage org.ginga.tools.TargetObsListSummary <target>");
             System.exit(1);
         }
         TargetObsListSummary summary = new TargetObsListSummary();
-        summary.execute(args[0]);
+        summary.printMpc2Modes(args[0]);
+        // summary.printSpectralModes(args[0]);
+        //summary.printAllModes(args[0]);
     }
 }
