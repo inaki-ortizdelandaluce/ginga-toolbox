685f5d2544417c3b21b0d4b94b541229910b35e6
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2015-01-04 03:34:13 +0100

    Naming

--- src/main/java/org/ginga/toolbox/pipeline/SpectrumHayashidaPipeline.java
+++ src/main/java/org/ginga/toolbox/pipeline/SpectrumHayashidaPipeline.java
@@ -14,7 +14,7 @@ import com.tinkerpop.pipes.util.Pipeline;
 public class SpectrumHayashidaPipeline extends Pipeline<SingleModeTargetObservation, File> {
 	
 	private Pipe<SingleModeTargetObservation, SingleModeTargetObservation> modeFilter;
-    private Pipe<SingleModeTargetObservation, LacdumpQuery> constraintsBuilder;
+    private Pipe<SingleModeTargetObservation, LacdumpQuery> queryBuilder;
     private Pipe<LacdumpQuery, LacqrdfitsInputModel> lacqrdfitsInputBuilder;
     private Pipe<LacqrdfitsInputModel, File> lacqrdfits;
     private Pipe<File, File> lac2xspec;
@@ -24,12 +24,12 @@ public class SpectrumHayashidaPipeline extends Pipeline<SingleModeTargetObservat
 		 // initialize all pipes needed
 	    this.modeFilter = new FilterFunctionPipe<SingleModeTargetObservation>(
 	            new SpectrumModeFilterPipe());
-	    this.constraintsBuilder = new LacdumpQueryPipe();
+	    this.queryBuilder = new LacdumpQueryPipe();
 	    this.lacqrdfitsInputBuilder = new LacqrdfitsInputPipe();
 	    this.lacqrdfits = new LacqrdfitsPipe();
 	    this.lac2xspec = new Lac2xspecPipe();
 	    this.pipeline = new Pipeline<SingleModeTargetObservation, File>(this.modeFilter, 
-	    		this.constraintsBuilder, 
+	    		this.queryBuilder, 
 	    		this.lacqrdfitsInputBuilder,
 	    		this.lacqrdfits,
 	    		this.lac2xspec);
