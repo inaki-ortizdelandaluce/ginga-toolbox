2943c13753d803d85bb836c1373e4dfa43a0445d
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2015-01-05 16:57:19 +0100

    New: Added database ingestion commands

---
+++ src/main/java/org/ginga/toolbox/command/LacdumpDatabaseIngestorCmd.java
@@ -0,0 +1,73 @@
+package org.ginga.toolbox.command;
+
+import java.io.File;
+import java.util.Comparator;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
+import org.ginga.toolbox.lacdump.LacdumpDatabaseIngestor;
+
+public class LacdumpDatabaseIngestorCmd {
+
+    public static final Logger log = Logger.getLogger(LacdumpDatabaseIngestorCmd.class);
+
+    public static void main(String[] args) {
+        try {
+            CommandLine commandLine = new BasicParser().parse(getOptions(), args);
+            // read command line argument values
+            // read command line argument values
+            File root = null;
+            if (commandLine.hasOption("d")) {
+                String dirPath = commandLine.getOptionValue("d");
+                root = new File(dirPath);
+            } else {
+                printHelp();
+                System.exit(0);
+            }
+            // write target list
+            LacdumpDatabaseIngestor ingestor = new LacdumpDatabaseIngestor();
+            ingestor.ingest(root);
+        } catch (ParseException e) {
+            log.error(e.getMessage());
+            printHelp();
+        }
+    }
+
+    private static void printHelp() {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.setOptionComparator(new Comparator<Option>() {
+
+            private static final String OPTS_ORDER = "dh"; // short option names
+
+            @Override
+            public int compare(Option o1, Option o2) {
+                String argCharOption1 = o1.getLongOpt().substring(0, 1);
+                String argCharOption2 = o2.getLongOpt().substring(0, 1);
+                return OPTS_ORDER.indexOf(argCharOption1) - OPTS_ORDER.indexOf(argCharOption2);
+            }
+        });
+        helpFormatter.printHelp(LacdumpDatabaseIngestorCmd.class.getCanonicalName(), getOptions());
+    }
+
+    @SuppressWarnings("static-access")
+    private static Options getOptions() {
+        Options options = new Options();
+        Option fileOption = OptionBuilder.withArgName("file path").withLongOpt("directory")
+                .withDescription("LACDUMP root directory").hasArg().create("d");
+        Option helpOption = new Option("h", "help", false, "show command help");
+
+        OptionGroup group = new OptionGroup();
+        group.setRequired(true);
+        group.addOption(helpOption);
+        group.addOption(fileOption);
+        options.addOptionGroup(group);
+        return options;
+    }
+}
--- src/main/java/org/ginga/toolbox/lacdump/LacdumpDatabaseIngestor.java
+++ src/main/java/org/ginga/toolbox/lacdump/LacdumpDatabaseIngestor.java
@@ -15,50 +15,43 @@ public class LacdumpDatabaseIngestor {
 
     private final static Logger log = Logger.getRootLogger();
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out
-                    .println("Usage org.ginga.toolbox.lacdump.DatabaseIngestor <LAC dump directory>");
+    public LacdumpDatabaseIngestor() {
+
+    }
+
+    public void ingest(File lacdumpRootDir) {
+        if (!lacdumpRootDir.exists()) {
+            log.error("Directory " + lacdumpRootDir.getPath() + " not found");
             System.exit(1);
-
-        } else {
-
-            File f = new File(args[0]);
-            if (!f.exists()) {
-                log.error("Directory " + f.getPath() + " not found");
-                System.exit(1);
-            } else if (!f.isDirectory()) {
-                log.error(f.getPath() + " is not a directory");
-                System.exit(1);
-            }
-
-            File[] lacdumpFiles = f.listFiles(new FileUtil.LacdumpFileFilter());
-            Arrays.sort(lacdumpFiles);
-
-            log.info(lacdumpFiles.length + " file(s) found");
-            LacdumpParser parser = new LacdumpParser();
-            LacdumpDao dao = new LacdumpDaoImpl();
-            List<LacdumpSfEntity> sfList = null;
-            for (int i = 0; i < lacdumpFiles.length; i++) {
-                try {
-                    sfList = parser.parse(lacdumpFiles[i]);
-                    log.info("LACDUMP contains " + sfList.size() + " row(s)");
-                    log.info("Inserting LACDUMP file " + lacdumpFiles[i]);
-                    dao.saveList(sfList);
-                } catch (IOException e) {
-                    log.error("Error parsing LACDUMP " + lacdumpFiles[i].getPath() + ". Message="
-                            + e.getMessage(), e);
-                    System.exit(1);
-                } catch (LacdumpDaoException e) {
-                    log.error("Error ingesting LACDUMP " + lacdumpFiles[i].getPath()
-                            + " into the database. Message=" + e.getMessage(), e);
-                    System.exit(1);
-                }
-            }
-            log.info(lacdumpFiles.length + " file(s) ingested into the database successfully");
+        } else if (!lacdumpRootDir.isDirectory()) {
+            log.error(lacdumpRootDir.getPath() + " is not a directory");
+            System.exit(1);
         }
+
+        File[] lacdumpFiles = lacdumpRootDir.listFiles(new FileUtil.LacdumpFileFilter());
+        Arrays.sort(lacdumpFiles);
+
+        log.info(lacdumpFiles.length + " file(s) found");
+        LacdumpParser parser = new LacdumpParser();
+        LacdumpDao dao = new LacdumpDaoImpl();
+        List<LacdumpSfEntity> sfList = null;
+        for (int i = 0; i < lacdumpFiles.length; i++) {
+            try {
+                sfList = parser.parse(lacdumpFiles[i]);
+                log.info("LACDUMP contains " + sfList.size() + " row(s)");
+                log.info("Inserting LACDUMP file " + lacdumpFiles[i]);
+                dao.saveList(sfList);
+            } catch (IOException e) {
+                log.error(
+                        "Error parsing LACDUMP " + lacdumpFiles[i].getPath() + ". Message="
+                                + e.getMessage(), e);
+                System.exit(1);
+            } catch (LacdumpDaoException e) {
+                log.error("Error ingesting LACDUMP " + lacdumpFiles[i].getPath()
+                        + " into the database. Message=" + e.getMessage(), e);
+                System.exit(1);
+            }
+        }
+        log.info(lacdumpFiles.length + " file(s) ingested into the database successfully");
     }
 }
