fa1a95f66a73029b6f997ee33fdb1686ff95ba5a
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2015-01-05 16:23:02 +0100

    New: Database Schema generator command

--- src/main/java/org/ginga/toolbox/util/DatabaseSchemaGenerator.java
+++ src/main/java/org/ginga/toolbox/util/DatabaseSchemaGenerator.java
@@ -4,10 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.ginga.toolbox.lacdump.LacdumpSfEntity;
-import org.ginga.toolbox.observation.ObservationBgEntity;
-import org.ginga.toolbox.observation.ObservationDataEntity;
-import org.ginga.toolbox.observation.ObservationEntity;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
@@ -18,8 +14,8 @@ public class DatabaseSchemaGenerator {
         MySQL("org.hibernate.dialect.MySQLDialect"), MySQLInno(
                 "org.hibernate.dialect.MySQLInnoDBDialect"), MySQLMyISAM(
                 "org.hibernate.dialect.MySQLMyISAMDialect"), MySQL5(
-                                "org.hibernate.dialect.MySQL5Dialect"), MySQL5Inno(
-                                        "org.hibernate.dialect.MySQL5InnoDBDialect");
+                "org.hibernate.dialect.MySQL5Dialect"), MySQL5Inno(
+                "org.hibernate.dialect.MySQL5InnoDBDialect");
 
         private String className;
 
@@ -58,15 +54,5 @@ public class DatabaseSchemaGenerator {
         boolean consolePrint = true;
         boolean exportInDatabase = false;
         schemaExport.create(consolePrint, exportInDatabase);
-    }
-
-    public static void main(String[] args) {
-        DatabaseSchemaGenerator schemaGen = new DatabaseSchemaGenerator();
-        schemaGen.generate(LacdumpSfEntity.class, new File("/tmp/lacdump-CREATE_TABLE.sql"));
-        List<Class<?>> annotatedClasses = new ArrayList<>();
-        annotatedClasses.add(ObservationEntity.class);
-        annotatedClasses.add(ObservationBgEntity.class);
-        annotatedClasses.add(ObservationDataEntity.class);
-        schemaGen.generate(annotatedClasses, new File("/tmp/observation-CREATE_TABLE.sql"));
     }
 }
