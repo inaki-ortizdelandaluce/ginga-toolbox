a612f7c4a4b52f70d699e3c0c6156cf8bae47d99
Inaki Ortiz de Landaluce <inaki.ortizdelandaluce@gmail.com>
2015-01-05 11:24:55 +0100

    Fix: File naming conventions must be different depending on LACDUMP query type (e.g. background constraints for GTI do not include start/end time)

--- src/main/java/org/ginga/toolbox/pipeline/LacspecInputPipe.java
+++ src/main/java/org/ginga/toolbox/pipeline/LacspecInputPipe.java
@@ -15,128 +15,127 @@ import org.ginga.toolbox.lacdump.dao.LacdumpDao;
 import org.ginga.toolbox.lacdump.dao.LacdumpDaoException;
 import org.ginga.toolbox.lacdump.dao.impl.LacdumpDaoImpl;
 import org.ginga.toolbox.lacspec.LacspecInputModel;
-import org.ginga.toolbox.util.FileUtil;
 import org.ginga.toolbox.util.Constants.BgSubtractionMethod;
+import org.ginga.toolbox.util.FileUtil;
 
 import com.tinkerpop.pipes.AbstractPipe;
 import com.tinkerpop.pipes.transform.TransformPipe;
 
-public abstract class LacspecInputPipe extends
-		AbstractPipe<LacdumpQuery, LacspecInputModel> implements
-		TransformPipe<LacdumpQuery, LacspecInputModel> {
+public abstract class LacspecInputPipe extends AbstractPipe<LacdumpQuery, LacspecInputModel>
+        implements TransformPipe<LacdumpQuery, LacspecInputModel> {
 
-	private final static Logger log = Logger
-			.getLogger(LacspecInputPipe.class);
+    private final static Logger log = Logger.getLogger(LacspecInputPipe.class);
 
-	public LacspecInputPipe() {
-	}
-	
-	public abstract boolean isBackground();
-	
-	public abstract BgSubtractionMethod getBgSubtractionMethod();
-	
-	public abstract String getBgFileName();
-	
-	/*
-	 * Receives a LacdumpQuery, creates a GTI/Region file and finally
-	 * emits a LacspecInputModel referencing such file
-	 */
-	@Override
-	protected LacspecInputModel processNextStart()
-			throws NoSuchElementException {
-		try {
-			LacdumpQuery query = this.starts.next();
+    public LacspecInputPipe() {
+    }
 
-			// set working directory
-			File workingDir = new File(GingaToolboxEnv.getInstance().getWorkingDir());
-			if (!workingDir.exists()) {
-				workingDir.mkdirs();
-			}
-			log.info("Working directory " + workingDir.getAbsolutePath());
+    public abstract boolean isBackground();
 
-			// build empty GTI file
-			File gtiFile = null;
-			if(isBackground()) {
-				gtiFile = new File(workingDir, FileUtil.nextFileName("BGD", 
-						query.getStartTime(), query.getMode(), "DATA"));
-			} else {
-				gtiFile = new File(workingDir, FileUtil.nextFileName("REGION", 
-						query.getStartTime(), query.getMode(), "DATA"));
-			}
+    public abstract BgSubtractionMethod getBgSubtractionMethod();
 
-			// query entities matching the criteria
-			LacdumpDao dao = new LacdumpDaoImpl();
-			List<LacdumpSfEntity> sfList = dao.findSfList(query);
-			log.info("Query executed successfully. " + sfList.size()
-					+ " result(s) found");
+    public abstract String getBgFileName();
 
-			if (sfList.size() > 0) {
-				// save matching results into a GTI file
-				GtiFileWriter gtiWriter = new GtiFileWriter();
-				gtiWriter.writeToFile(query.getTargetName(), sfList, false, gtiFile);
-				log.debug("GTI file " + gtiFile.getPath()
-						+ " written successfully");
+    /*
+     * Receives a LacdumpQuery, creates a GTI/Region file and finally emits a LacspecInputModel
+     * referencing such file
+     */
+    @Override
+    protected LacspecInputModel processNextStart() throws NoSuchElementException {
+        try {
+            LacdumpQuery query = this.starts.next();
 
-				// emit lacspec input model
-				LacspecInputModel inputModel = new LacspecInputModel();
-				DataReductionEnv env = GingaToolboxEnv.getInstance().getDataReductionEnv();
-				inputModel.setHasBackground(!isBackground());
-				if(!isBackground()) {
-					inputModel.setBgMethod(getBgSubtractionMethod());
-					inputModel.setBgFileName(getBgFileName());
-					inputModel.setBgSubFileNumber(env.getBgSubFileNumber());
-				}
-	            inputModel.setLacMode(query.getMode());
-	            inputModel.setBitRate(env.getBitRate());
-	            if(isBackground()) {
-	            	inputModel.setPsFileName(FileUtil.nextFileName("lacspec_bgd",
-						query.getStartTime(), query.getMode(), "ps"));
-	            } else {
-	            	inputModel.setPsFileName(FileUtil.nextFileName("lacspec",
-						query.getStartTime(), query.getMode(), "ps"));
-	            }
-	            inputModel.setMinElevation(env.getElevationMin());
-	            inputModel.setMaxElevation(env.getElevationMax());
-	            inputModel.setMinRigidity(env.getCutOffRigidityMin());
-	            inputModel.setMaxRigidity(env.getCutOffRigidityMax());
-	            if(isBackground()) {
-	            	inputModel.setBgCorrection(0);
-	            } else {
-	            	inputModel.setBgCorrection(1);
-	            }
-	            inputModel.setAspectCorrection(env.getAspectCorrection());
-	            inputModel.setDeadTimeCorrection(env.getDeadTimeCorrection());
-	            inputModel.setChannelToEnergy(env.getChannelToEnergyConversion());
-	            inputModel.setDataUnit(env.getDataUnit());
-	            inputModel.setAce(env.getAttitudeMode());
-	            inputModel.setCounter1(env.getLacCounter1());
-	            inputModel.setCounter2(env.getLacCounter2());
-	            inputModel.setCounter3(env.getLacCounter3());
-	            inputModel.setCounter4(env.getLacCounter4());
-	            inputModel.setCounter5(env.getLacCounter5());
-	            inputModel.setCounter6(env.getLacCounter6());
-	            inputModel.setCounter7(env.getLacCounter7());
-	            inputModel.setCounter8(env.getLacCounter8());
-	            inputModel.setMixedMode(env.isLacMixedMode());
-	            inputModel.setRegionFileName(gtiFile.getName());
-	            inputModel.setMonitorFileName("MONI.SPEC");
-	            if(isBackground()) {
-	            	inputModel.setSpectralFileName(FileUtil.nextFileName("SPEC_BGD",
-						query.getStartTime(), query.getMode(), "FILE"));
-	            	inputModel.setMonitorFileName(FileUtil.nextFileName("MONI_BGD",
-						query.getStartTime(), query.getMode(), "SPEC"));
-	            } else {
-	            	inputModel.setSpectralFileName(FileUtil.nextFileName("SPEC",
-							query.getStartTime(), query.getMode(), "FILE"));
-	            	inputModel.setMonitorFileName(FileUtil.nextFileName("MONI",
-						query.getStartTime(), query.getMode(), "SPEC"));
-	            }
-				return inputModel;
-			}
-		} catch (IOException | LacdumpDaoException e) {
-			log.error("Error generating GTI file. Message= " + e.getMessage(),
-					e);
-		}
-		return null;
-	}
+            // set working directory
+            File workingDir = new File(GingaToolboxEnv.getInstance().getWorkingDir());
+            if (!workingDir.exists()) {
+                workingDir.mkdirs();
+            }
+            log.info("Working directory " + workingDir.getAbsolutePath());
+
+            // build empty GTI file
+            File gtiFile = null;
+            if (isBackground()) {
+                log.debug("Generating GTI file for on-source data");
+                String prefix = query.getTargetName().replace(" ", "") + "_BGD";
+                gtiFile = new File(workingDir, FileUtil.nextFileName(workingDir, prefix, "DATA"));
+            } else {
+                log.debug("Generating GTI file for on-source data");
+                gtiFile = new File(workingDir, FileUtil.nextFileName("REGION",
+                        query.getStartTime(), query.getMode(), "DATA"));
+            }
+
+            log.debug("GTI file " + gtiFile.getPath());
+
+            // query entities matching the criteria
+            LacdumpDao dao = new LacdumpDaoImpl();
+            List<LacdumpSfEntity> sfList = dao.findSfList(query);
+            log.info("Query executed successfully. " + sfList.size() + " result(s) found");
+
+            if (sfList.size() > 0) {
+                // save matching results into a GTI file
+                GtiFileWriter gtiWriter = new GtiFileWriter();
+                gtiWriter.writeToFile(query.getTargetName(), sfList, true, gtiFile);
+                log.info("GTI file " + gtiFile.getPath() + " written successfully");
+
+                // emit lacspec input model
+                LacspecInputModel inputModel = new LacspecInputModel();
+                DataReductionEnv env = GingaToolboxEnv.getInstance().getDataReductionEnv();
+                inputModel.setHasBackground(!isBackground());
+                if (!isBackground()) {
+                    inputModel.setBgMethod(getBgSubtractionMethod());
+                    inputModel.setBgFileName(getBgFileName());
+                    inputModel.setBgSubFileNumber(env.getBgSubFileNumber());
+                }
+                inputModel.setLacMode(query.getMode());
+                inputModel.setBitRate(env.getBitRate());
+                if (isBackground()) {
+                    String prefix = query.getTargetName().replace(" ", "") + "_lacspec_bgd";
+                    inputModel.setPsFileName(FileUtil.nextFileName(workingDir, prefix, "ps"));
+                } else {
+                    inputModel.setPsFileName(FileUtil.nextFileName("lacspec", query.getStartTime(),
+                            query.getMode(), "ps"));
+                }
+                inputModel.setMinElevation(env.getElevationMin());
+                inputModel.setMaxElevation(env.getElevationMax());
+                inputModel.setMinRigidity(env.getCutOffRigidityMin());
+                inputModel.setMaxRigidity(env.getCutOffRigidityMax());
+                if (isBackground()) {
+                    inputModel.setBgCorrection(0);
+                } else {
+                    inputModel.setBgCorrection(1);
+                }
+                inputModel.setAspectCorrection(env.getAspectCorrection());
+                inputModel.setDeadTimeCorrection(env.getDeadTimeCorrection());
+                inputModel.setChannelToEnergy(env.getChannelToEnergyConversion());
+                inputModel.setDataUnit(env.getDataUnit());
+                inputModel.setAce(env.getAttitudeMode());
+                inputModel.setCounter1(env.getLacCounter1());
+                inputModel.setCounter2(env.getLacCounter2());
+                inputModel.setCounter3(env.getLacCounter3());
+                inputModel.setCounter4(env.getLacCounter4());
+                inputModel.setCounter5(env.getLacCounter5());
+                inputModel.setCounter6(env.getLacCounter6());
+                inputModel.setCounter7(env.getLacCounter7());
+                inputModel.setCounter8(env.getLacCounter8());
+                inputModel.setMixedMode(env.isLacMixedMode());
+                inputModel.setRegionFileName(gtiFile.getName());
+                inputModel.setMonitorFileName("MONI.SPEC");
+                if (isBackground()) {
+                    String prefix = query.getTargetName().replace(" ", "");
+                    inputModel.setSpectralFileName(FileUtil.nextFileName(workingDir, prefix
+                            + "_SPEC_BGD", "FILE"));
+                    inputModel.setMonitorFileName(FileUtil.nextFileName(workingDir, prefix
+                            + "_MONI_BGD", "SPEC"));
+                } else {
+                    inputModel.setSpectralFileName(FileUtil.nextFileName("SPEC",
+                            query.getStartTime(), query.getMode(), "FILE"));
+                    inputModel.setMonitorFileName(FileUtil.nextFileName("MONI",
+                            query.getStartTime(), query.getMode(), "SPEC"));
+                }
+                return inputModel;
+            }
+        } catch (IOException | LacdumpDaoException e) {
+            log.error("Error generating GTI file. Message= " + e.getMessage(), e);
+        }
+        return null;
+    }
 }
--- src/main/java/org/ginga/toolbox/pipeline/LacspecPipe.java
+++ src/main/java/org/ginga/toolbox/pipeline/LacspecPipe.java
@@ -14,62 +14,64 @@ import org.ginga.toolbox.util.FileUtil;
 import com.tinkerpop.pipes.AbstractPipe;
 import com.tinkerpop.pipes.transform.TransformPipe;
 
-public class LacspecPipe extends AbstractPipe<LacspecInputModel, File>
-		implements TransformPipe<LacspecInputModel, File> {
+public class LacspecPipe extends AbstractPipe<LacspecInputModel, File> implements
+        TransformPipe<LacspecInputModel, File> {
 
-	private static final Logger log = Logger.getLogger(LacspecPipe.class);
+    private static final Logger log = Logger.getLogger(LacspecPipe.class);
 
-	/*
-	 * Receives a LacspecInputModel, writes it to an input file, executes the
-	 * lacspec routine and finally emits the resulting spectrum file
-	 */
-	@Override
-	protected File processNextStart() throws NoSuchElementException {
-		try {
-			LacspecInputModel inputModel = this.starts.next();
-			if (inputModel != null) {
-				GingaToolboxEnv env = GingaToolboxEnv
-						.getInstance();
-				File workingDir = new File(env.getWorkingDir());
-				if (!workingDir.exists()) {
-					workingDir.mkdirs();
-				}
-				log.info("Working directory " + workingDir.getAbsolutePath());
+    /*
+     * Receives a LacspecInputModel, writes it to an input file, executes the lacspec routine and
+     * finally emits the resulting spectrum file
+     */
+    @Override
+    protected File processNextStart() throws NoSuchElementException {
+        try {
+            LacspecInputModel inputModel = this.starts.next();
+            if (inputModel != null) {
+                GingaToolboxEnv env = GingaToolboxEnv.getInstance();
+                File workingDir = new File(env.getWorkingDir());
+                if (!workingDir.exists()) {
+                    workingDir.mkdirs();
+                }
+                log.info("Working directory " + workingDir.getAbsolutePath());
 
-				// create input file
-				File inputFile = new File(workingDir, FileUtil.nextFileName(
-						"lacspec", inputModel.getStartTime(), inputModel.getLacMode(), "input"));
-				LacspecInputFileWriter lacspecInputFileWriter = 
-						new LacspecInputFileWriter(inputModel);
-				lacspecInputFileWriter.writeToFile(inputFile);
-				log.info("Input file " + inputFile.getPath()
-						+ " created successfully");
+                // create input file
+                File inputFile = null;
+                if (inputModel.getHasBackground()) {
+                    inputFile = new File(workingDir, FileUtil.nextFileName("lacspec",
+                            inputModel.getStartTime(), inputModel.getLacMode(), "input"));
+                } else {
+                    inputFile = new File(workingDir, FileUtil.nextFileName(workingDir,
+                            "lacspec_bgd", "input"));
+                }
+                LacspecInputFileWriter lacspecInputFileWriter = new LacspecInputFileWriter(
+                        inputModel);
+                lacspecInputFileWriter.writeToFile(inputFile);
+                log.info("Input file " + inputFile.getPath() + " created successfully");
 
-				// create output file
-				File outputFile = new File(workingDir,
-						FileUtil.splitFileBaseAndExtension(inputFile)[0]
-								+ ".log");
+                // create output file
+                File outputFile = new File(workingDir,
+                        FileUtil.splitFileBaseAndExtension(inputFile)[0] + ".log");
 
-				// create 'lacspec' command
-				String cmd = env.getGingaToolsBinDir() + "/lacspec";
+                // create 'lacspec' command
+                String cmd = env.getGingaToolsBinDir() + "/lacspec";
 
-				// execute command
-				GingaToolsRuntime runtime = new GingaToolsRuntime(workingDir,
-						inputFile, outputFile, cmd);
-				log.info("Executing command " + cmd + " ...");
-				int exitValue = runtime.exec();
-				log.debug("Exit value " + exitValue);
-				if (exitValue == 0) { // return 'lacqrdfits' output file
-					log.info("Command executed successfully");
-					return new File(workingDir,
-							inputModel.getSpectralFileName());
-				} else {
-					log.error("Error executing command " + cmd);
-				}
-			}
-		} catch (IOException e) {
-			log.error("Error generating spectrum with lacspec", e);
-		}
-		return null;
-	}
+                // execute command
+                GingaToolsRuntime runtime = new GingaToolsRuntime(workingDir, inputFile,
+                        outputFile, cmd);
+                log.info("Executing command " + cmd + " ...");
+                int exitValue = runtime.exec();
+                log.debug("Exit value " + exitValue);
+                if (exitValue == 0) { // return 'lacqrdfits' output file
+                    log.info("Command executed successfully");
+                    return new File(workingDir, inputModel.getSpectralFileName());
+                } else {
+                    log.error("Error executing command " + cmd);
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error generating spectrum with lacspec", e);
+        }
+        return null;
+    }
 }
